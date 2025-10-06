63379ec42b6dfad2aa54a3265dcf3b118e9a5fdb
prasad0108-ux <suryavanshipr30@gmail.com>
2025-10-06 22:35:37 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-demo-ccprasad "
+    bucket = "terraform-demo-ccprasad"
     key    = "simple-app/terraform.tfstate"
     region = "us-east-1"
     encrypt = true
