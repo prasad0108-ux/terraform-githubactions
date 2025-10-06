64fe9f1231790d31d46a4044656472baf1c70e1c
prasad0108-ux <suryavanshipr30@gmail.com>
2025-10-06 22:42:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "main" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.1.0/24"
-  availability_zone = "ap-south-1a"
+  availability_zone = "us-east-1a"
   tags = { Name = "simple-subnet" }
 }
 
